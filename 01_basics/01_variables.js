36563ec19d88df9f694448beaaf32b4ebdf30697
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-05 22:25:33 +0000

    conatainer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1344
+let accoundEmail="ayush@gmail.com"
+var accountPassword='123445'
+accountCity="Jaipur"
+let accountState;
+
+// accountId=2    // Not allowed
+accoundEmail="hccs@.ccom"
+accountPassword="3334455"
+accountCity="Bengaluru"
+console.log(accountId);
+console.table([accountId,accoundEmail,accountPassword,accountCity,accountState]);
