35c471f4a00cb95539af7d5f0c450d937fa3533b
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-06 08:37:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all JS code as newer version
+// alert("hello")
+ // we are usinotg node js not browser
+console.log(3+3);
+console.log("ayush")
+
+let name="Honey"
+let age=19
+let isLoggedIn=false
+//number=>2 to power 53
+//bigint
+//string=> ""
+//boolean=>true/false
+// null=> standolone value
+//undefined=>
+//symbol=>unique
+
+
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
