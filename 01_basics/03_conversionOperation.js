a7714c5482ecf58d2815edd487462d65dae73c11
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-07 06:33:27 +0000

    operations on datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ let valueNumber=Number(score);
 
 let isLoggedIn = "ayush"
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=>true;0=>false;
 //""=> false
@@ -22,6 +22,45 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// *****************************Operations******************************//
+
+ let value=3
+ let negValue=-value;
+//  console.log(negValue);
+ 
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+
+ let str1="Hello"
+ let str2=" Hitesh"
+ let str3=str1+str2
+//  console.log(str3);
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log("1"+2+2);
+ 
+//  console.log(1+2+"2");
+ 
+//  console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100
+
+++gameCounter;
+console.log(gameCounter);
+ 
+ 
+ 
