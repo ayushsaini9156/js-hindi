79dd221c53cf6c41ae7cf6036cec98dd5141b7ad
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-07 10:28:42 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+// # Primitive datatype
+// 7 Types: String, Number , Boolean , null , undefined ,Symbol , BigInt
+
+const value=100
+const newValue=100.2
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;  // undefined 
+  
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+const bigNumber= 34455559585898385098509n
+
+// Reference or non- Primitive datatype
+
+// Array, Objects, Functions
+
+const heroes = ["Shaktiman","naagraj","doga"]
+let myObj={
+    name:"Ayush",
+    age:23,
+
+}
+
+const myFunc =function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
