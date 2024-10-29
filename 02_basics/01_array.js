668b9c4c3bcd8effe9f351cbe510dfb08126d2bb
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-29 11:38:13 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,29 @@
+//Array
+
+let arr=[3,4,5,6,7];
+console.log(arr[3]);
+
+
+const myHereos = ["Shaktimaan","naagraj"]
+const myArr=new Array(1,2,3,4,5)
+
+console.log(myArr[0]);
+
+//Array Methods 
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(0)
+// myArr.shift()
+
+
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(1));
+
+
+const newArr = myArr.join
+console.log(myArr);
+
+
