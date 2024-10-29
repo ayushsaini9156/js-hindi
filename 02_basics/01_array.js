450bbdf0c47ee5a0ccbe71142d1aa0943bf9f2dc
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-29 11:45:35 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -24,7 +24,23 @@ const myArr=new Array(1,2,3,4,5)
 
 
 const newArr = myArr.join()
-console.log(myArr);
-console.log(typeof newArr);
+// console.log(myArr);
+// console.log(typeof newArr);
+
+//slice ,splice
+
+console.log("A",myArr);
+const myn1=myArr.slice(1,3);
+
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("C",myArr);
+
+console.log(myn2);
+
+
+
 
 
