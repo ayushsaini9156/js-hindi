f11d52389c8cf796d0825e60f0619787fa4c83e1
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-10-31 15:01:53 +0000

    function in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,8 +27,36 @@ function loginUserMessage(username="sam"){
         console.log("Please enter a username:");
         return 
     }
-    return `${username} just logged in  `
+    return `${username} just logged in `
 }
 // console.log(loginUserMessage("hitesh"))
 console.log(loginUserMessage("ayush"))
 
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+} 
+
+console.log(calculateCartPrice(200,400,500,2000));
+
+
+const user= {
+    username:"hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+const myNewArray =[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue([200,400,500,1000]));
