7f4ca465b004483b2c71fae83a05203ea4129d85
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-11-09 09:12:34 +0000

    for loop

---
+++ 05_iterators/one.js
@@ -0,0 +1,13 @@
+// for
+
+for (let i = 0; i<=10; i++) {
+    console.log(`outer loop: ${i}`);
+    
+    for (let j = 0; j<=10; j++) {
+        
+        console.log(`Inner loop value ${j} and outer loop ${i}`);
+        
+    }
+    
+}
+
