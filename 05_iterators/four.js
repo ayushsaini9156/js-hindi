218627ce53c24fbc043a2fabdf46489ba8cc9f35
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-11-09 09:37:41 +0000

    for in loop

---
+++ 05_iterators/four.js
@@ -0,0 +1,10 @@
+// const myObj={
+//     js:'javascript',
+//     cpp:'c++',
+//     ruby:'ruby',swift:'swift'
+// }
+// for (const key in myObj) {
+//    console.log(`${key} shortcut is for ${myObj[key]}`);
+   
+// }
+
