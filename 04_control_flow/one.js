3190e2c152ddf254807477669e209ade4ad2239d
ayushsaini9156 <131479937+ayushsaini9156@users.noreply.github.com>
2024-11-09 06:05:53 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+// if statement
+
+// if(2=="2"){
+// console.log("executed");
+// }
+
+// const score = 200
+
+// if(score>100){
+//     let power= 'fly'
+//     console.log(`use power: ${power}`);
+    
+// }
+// console.log(`use power: ${power}`);
+
+const balance =1000
+
+// if(balance>500) console.log("test"), console.log("test2");
+
+// if(balance<500){
+//     console.log("less than 500");
+    
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// } 
+// else if(balance<900){
+//     console.log("less than 900");
+// } 
+// else{
+//     console.log("less than 1200");
+    
+// }
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle= false
+const loggedInFromEmail=true
+if(userLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+if(loggedInFromGoogle || loggedInFromEmail ){
+    console.log("user logged in ");
+    
+}
